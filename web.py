64950f8835e16c29cc7a77f606671ca04aaff756
Trainwagon <pangeranmlg278@gmail.com>
2024-11-17 19:31:14 +0700

    improve the UX.

--- web.py
+++ web.py
@@ -5,21 +5,23 @@ todos = functions.get_todos()
 
 
 def add_todo():
-    todo = st.session_state['new_todo'] + "\n"
-    todos.append(todo)
-    functions.write_todos(todos)
+    todo = st.session_state['new_todo'].strip()
+    if todo:
+        todos.append(todo  + "\n")
+        functions.write_todos(todos)
+        st.session_state['new_todo'] = ""
 
 
 st.title("Todo app")
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    checkbox = st.checkbox(todo, key=f"{todo}_{index}")
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del st.session_state[todo]
+        del st.session_state[f"{todo}_{index}"]
         st.rerun()
 
-st.text_input(label=" ", placeholder="add a new todo", on_change=add_todo, key='new_todo')
-
+st.text_input(label="Add a new todo:", placeholder="Type here...", on_change=add_todo, key='new_todo')
+st.write("---")
 # st.session_state
